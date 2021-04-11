49d672582bbddc495bcfe78f85b2883c3a6c2a18
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-04-11 17:24:38 -0400

    refacto

--- src/data/config.js
+++ src/data/config.js
@@ -1,34 +1,25 @@
 module.exports = {
   defaultTitle: "Portfolio d'Arnaud Flaesch",
-  logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
+  logo: './static/favicon/favicon.ico',
   author: 'Arnaud Flaesch',
   url: 'https://arnaudflaesch.github.io/',
   legalName: 'Arnaud Flaesch',
   defaultDescription: "I'm Arnaud and I'm a Software Developer!",
   socialLinks: {
-    twitter: 'http://www.twitter.com/smakosh',
-    github: 'https://github.com/smakosh',
-    linkedin: 'https://www.linkedin.com/in/ismail-ghallou-630149122/',
-    instagram: 'https://instagram.com/smakosh19',
-    youtube: 'https://www.youtube.com/user/smakoshthegamer',
-    google: 'https://plus.google.com/u/0/+IsmailSmakoshGhallou',
+    github: 'https://github.com/ArnaudFlaesch',
+    linkedin: 'https://www.linkedin.com/in/arnaudflaesch/'
   },
   googleAnalyticsID: 'UA-88875900-4',
   themeColor: '#6b63ff',
   backgroundColor: '#6b63ff',
-  social: {
-    facebook: 'appId',
-    twitter: '@smakosh',
-  },
   address: {
-    city: 'City',
-    region: 'Region',
-    country: 'Country',
-    zipCode: 'ZipCode',
+    city: 'Paris',
+    region: "Île-de-France",
+    country: 'France'
   },
   contact: {
     email: 'email',
     phone: 'phone number',
   },
-  foundingDate: '2018',
+  foundingDate: '2021',
 };
