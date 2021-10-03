2a35d28b23d138e4bf62305d5612246465ec3d7b
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-10-03 16:27:48 +0200

    Fix tests

--- cypress/integration/tests/index.spec.ts
+++ cypress/integration/tests/index.spec.ts
@@ -6,24 +6,13 @@ describe('Portfolio tests', () => {
   });
 
   it('Should get the title', () => {
-    cy.title().should('equals', "Portfolio d'Arnaud Flaesch");
-    cy.get('h1').first().should('have.text', 'Hi There!');
-    cy.get('h4')
-      .first()
-      .should('have.text', "I'm Arnaud and I'm a JAMStack engineer!");
-  });
-
-  it('Should toggle between light and dark mode', () => {
-    cy.get('#themeIconToggle')
-      .click()
-      .get('body')
-      .should('have.css', 'color', 'rgb(255, 255, 255)')
-      .get('#themeIconToggle')
-      .click()
-      .should('have.css', 'color', 'rgb(0, 0, 0)');
+    cy.title().should('equals', 'Home | Arnaud Flaesch, Développeur Web');
+    cy.get('h1').first().should('have.text', 'Arnaud Flaesch, Développeur Web');
   });
 
   it('Should display the 404 error page', () => {
-    cy.visit('/404').title().should('equals', '404: Not found');
+    cy.visit('/404')
+      .title()
+      .should('equals', '404: Not found | Arnaud Flaesch, Développeur Web');
   });
 });
