8a824b1dcc2f34bb253df10d4c6f1f250628481c
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-10-06 21:28:03 +0200

    Revert "no 404"
    This reverts commit fa1dcad2f420aa840c4051afe1358f5b0dbd30a4.

---
+++ cypress/integration/tests/404.spec.ts
@@ -0,0 +1,9 @@
+/// <reference types='cypress' />
+
+describe('404 page tests', () => {
+  it('Should display the 404 error page', () => {
+    cy.visit('/404')
+      .title()
+      .should('equals', '404: Not found | Arnaud Flaesch, Développeur Web');
+  });
+});
