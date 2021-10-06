5b102fc47c10d0a176f3b85c31b5dba5ad1a3242
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-10-06 21:24:27 +0200

    404

--- cypress/integration/tests/index.spec.ts
+++ cypress/integration/tests/index.spec.ts
@@ -9,10 +9,4 @@ describe('Portfolio tests', () => {
     cy.title().should('equals', 'Home | Arnaud Flaesch, Développeur Web');
     cy.get('h1').first().should('have.text', 'Arnaud Flaesch, Développeur Web');
   });
-
-  it('Should display the 404 error page', () => {
-    cy.visit('/404')
-      .title()
-      .should('equals', '404: Not found | Arnaud Flaesch, Développeur Web');
-  });
 });
