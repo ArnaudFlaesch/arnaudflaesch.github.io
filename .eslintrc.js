cb6325e8d72b477498966c0705ddb76ab0939649
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-11-04 22:36:27 +0100

    Fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,6 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react-hooks'],
-  extends: [
-    'eslint:recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'plugin:cypress/recommended'
-  ],
-  rules: {
-    'react-hooks/rules-of-hooks': 'off',
-    'react-hooks/exhaustive-deps': 'off'
-  }
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:cypress/recommended']
 };
