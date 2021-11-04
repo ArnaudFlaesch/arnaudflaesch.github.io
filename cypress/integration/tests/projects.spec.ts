ddb65f5ade0c4a26cccb5835bf015b0ee2eaa769
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-11-04 22:43:58 +0100

    fix

--- cypress/integration/tests/projects.spec.ts
+++ cypress/integration/tests/projects.spec.ts
@@ -1,7 +1,7 @@
 /// <reference types='cypress' />
 
 describe('Projects tests', () => {
-  const PROJECTS_TITLES = ['FunChess', 'Dash-Web', 'Dash-WebServices', 'WizardLibrary'];
+  const PROJECTS_TITLES = ['Dash-Web', 'Dash-WebServices', 'WizardLibrary'];
 
   beforeEach(() => {
     cy.visit('/projects');
