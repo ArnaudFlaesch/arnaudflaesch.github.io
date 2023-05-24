920f43316e8840a2eb121abf25b62c6bcc32d448
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2023-05-24 20:26:00 +0200

    update

--- src/data/JobData.ts
+++ src/data/JobData.ts
@@ -2,9 +2,31 @@ import Experience from '../components/curriculum/Experience';
 
 const jobData: Experience[] = [
   {
+    id: 9,
+    title: 'Software Engineer',
+    dateDebut: new Date(Date.parse('2021-08-02')),
+    name: 'Publicis Sapient France',
+    location: 'Paris - 75020',
+    description: 'CDI',
+    skills: ['Angular 2', 'Java 8', 'SQL', 'Spring Boot'],
+    isCurrentExperience: true
+  },
+  {
+    id: 8,
+    title: 'Développeur Java / JS',
+    dateDebut: new Date(Date.parse('2019-05-01')),
+    dateFin: new Date(Date.parse('2021-04-30')),
+    name: 'tbMaestro',
+    location: 'Montréal - Canada',
+    description: 'V.I.E.',
+    skills: ['Angular 2', 'Java 8', 'SQL', 'Spring Boot'],
+    isCurrentExperience: false
+  },
+  {
     id: 7,
     title: 'Développeur Java / JS',
-    dateDebut: new Date(2017, 9, 18),
+    dateDebut: new Date(Date.parse('2017-08-18')),
+    dateFin: new Date(Date.parse('2018-04-12')),
     name: 'Sopra Steria',
     location: 'Montreuil - 93340',
     description:
@@ -22,14 +44,14 @@ const jobData: Experience[] = [
       'Sass',
       'Git'
     ],
-    isCurrentExperience: true
+    isCurrentExperience: false
   },
   {
     id: 6,
     title: 'Développeur Java',
     name: 'Franprix - Leader Price',
-    dateDebut: new Date(2016, 10, 1),
-    dateFin: new Date(2017, 8, 31),
+    dateDebut: new Date(Date.parse('2015-10-01')),
+    dateFin: new Date(Date.parse('2017-08-31')),
     location: 'Chennevières sur Marne - 94190',
     description: "Développeur Liferay / Alternance dans le cadre de ma deuxième année de mastère à l'ESGI.",
     skills: ['Java 7', 'Vaadin 7', 'Liferay', 'JSF 2', 'Tomcat 7', 'SVN']
@@ -38,8 +60,8 @@ const jobData: Experience[] = [
     id: 5,
     title: 'Développeur Web',
     name: 'AB Cube',
-    dateDebut: new Date(2015, 10, 1),
-    dateFin: new Date(2016, 8, 31),
+    dateDebut: new Date(Date.parse('2015-10-01')),
+    dateFin: new Date(Date.parse('2015-08-31')),
     location: 'Montreuil - 93340',
     description: "Développeur Web / Alternance dans le cadre de ma première année de mastère à l'ESGI.",
     skills: ['PHP', 'SQL', 'JavaScript', 'Git']
@@ -48,8 +70,8 @@ const jobData: Experience[] = [
     id: 4,
     title: 'Développeur Java',
     name: 'GMF Assurances',
-    dateDebut: new Date(2015, 5, 15),
-    dateFin: new Date(2015, 7, 31),
+    dateDebut: new Date(Date.parse('2015-05-15')),
+    dateFin: new Date(Date.parse('2015-07-31')),
     location: 'Levallois',
     description: "Développeur Java / Stage de trois mois dans le cadre de ma troisième année de bachelor à l'ESGI.",
     skills: ['Java 7', 'JSF 2', 'Struts 1', 'Git']
@@ -58,8 +80,8 @@ const jobData: Experience[] = [
     id: 3,
     title: 'Développeur Java / JS',
     name: 'La Réserve',
-    dateDebut: new Date(2014, 6, 1),
-    dateFin: new Date(2014, 7, 31),
+    dateDebut: new Date(Date.parse('2014-06-02')),
+    dateFin: new Date(Date.parse('2014-07-31')),
     location: 'Paris 08è',
     description: 'Stage de deux mois ',
     skills: ['PHP', 'SQL', 'JavaScript', 'JQuery']
@@ -68,8 +90,8 @@ const jobData: Experience[] = [
     id: 2,
     title: 'Développeur Web',
     name: 'Accueil Cambodgien',
-    dateDebut: new Date(2014, 1, 2),
-    dateFin: new Date(2014, 2, 13),
+    dateDebut: new Date(Date.parse('2014-01-13')),
+    dateFin: new Date(Date.parse('2014-02-14')),
     location: 'Saint-Mandé 94300',
     description: "Réalisation d'un site web e-commerce pour une association.",
     skills: ['Wordpress', 'PHP', 'SQL']
@@ -78,8 +100,8 @@ const jobData: Experience[] = [
     id: 1,
     title: 'Développeur PHP',
     name: 'Free / MCRA',
-    dateDebut: new Date(2013, 5, 31),
-    dateFin: new Date(2013, 6, 27),
+    dateDebut: new Date(Date.parse('2013-05-31')),
+    dateFin: new Date(Date.parse('2013-06-27')),
     location: 'Paris 08è',
     description: "Stage d'un mois dans le cadre de ma première année de BTS SIO.",
     skills: ['PHP', 'SQL', 'JavaScript']
