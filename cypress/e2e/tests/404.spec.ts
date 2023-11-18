0834d17195badd798b94cc597ba67b6a90127f80
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2023-11-18 15:29:22 +0100

    test

--- cypress/e2e/tests/404.spec.ts
+++ cypress/e2e/tests/404.spec.ts
@@ -1,10 +1,6 @@
 /// <reference types='cypress' />
 
 describe('404 page tests', () => {
-  beforeEach(() => {
-    cy.visit('/');
-  });
-
   it('Should display the 404 error page', () => {
     cy.visit('/404').title().should('equals', '404: Not found | Arnaud Flaesch, Développeur Web');
   });
