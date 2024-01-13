0444a4e46f56f77432677f31c4ca2eff1da97ba9
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2024-01-13 19:04:13 +0100

    remove test

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -3,23 +3,23 @@ import * as React from 'react';
 
 export default function Contact(): React.ReactElement {
   const [state, handleSubmit] = useForm('mkndgrkd');
-  if (state.succeeded) {
-    return <p>Thanks for joining!</p>;
-  }
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Votre adresse mail</label>
-        <input id="email" type="email" name="email" />
-        <ValidationError prefix="Email" field="email" errors={state.errors} />
-        <label htmlFor="message">Message</label>
-        <textarea id="message" name="message" />
-        <ValidationError prefix="Message" field="message" errors={state.errors} />
-        <button type="submit" disabled={state.submitting}>
-          Envoyer
-        </button>
-      </form>
+      {state.succeeded && <p>Votre email a bien été envoyé !</p>}
+      {!state.succeeded && (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Votre adresse mail</label>
+          <input id="email" type="email" name="email" />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+          <label htmlFor="message">Message</label>
+          <textarea id="message" name="message" />
+          <ValidationError prefix="Message" field="message" errors={state.errors} />
+          <button type="submit" disabled={state.submitting}>
+            Envoyer
+          </button>
+        </form>
+      )}
     </div>
   );
 }
