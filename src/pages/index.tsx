e4b19f159359e01fa5d59a08949b5a54b2a38234
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2024-01-20 15:11:01 +0100

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,8 @@ import Post from '../components/post/Post';
 import Seo from '../components/seo';
 import { IPageProps } from '../model/IPageProps';
 import { IPost } from '../model/IPost';
-import Cv from '../components/cv/Cv';
 import Skills from '../components/skills/Skills';
+import Cv from '../components/cv/Cv';
 
 export default function Index(props: Readonly<IPageProps>): React.ReactElement {
   const posts = props.data.allMarkdownRemark.nodes;
