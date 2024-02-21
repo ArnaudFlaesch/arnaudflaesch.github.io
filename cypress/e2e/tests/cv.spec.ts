a9cef7e27582934de3cd404b115c21ce5dba511d
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2024-02-21 20:59:14 +0100

    update

--- cypress/e2e/tests/cv.spec.ts
+++ cypress/e2e/tests/cv.spec.ts
@@ -1,8 +1,38 @@
 /// <reference types='cypress' />
+import path = require('path');
 
 describe('CV page tests', () => {
   it('Should display the CV page', () => {
     cy.visit('/').get('#portfolio-header > #url-list > a').contains('CV').click();
     cy.title().should('equals', 'Curriculum Vitae - Arnaud Flaesch, Développeur Web');
+
+    // Download CV
+    const cvFilename = 'Curriculum Vitae Arnaud Flaesch.pdf';
+    cy.readFile(path.join(Cypress.config('downloadsFolder'), cvFilename)).should('not.exist');
+    cy.get('#cv-download-button').should('have.text', 'Télécharger mon CV').click();
+    cy.readFile(path.join(Cypress.config('downloadsFolder'), cvFilename));
+
+    const blockTitles = [
+      'Développeur backend Java',
+      'Développeur fullstack',
+      'Développeur fullstack',
+      'Mastère Architecture des Logiciels',
+      'Bachelor Architecture des Logiciels',
+      'Lycée privé Saint Michel de Picpus',
+      'Brevet de Technicien Supérieur',
+      'Licence Mathématiques - Informatique',
+      'Lycée privé Massillon',
+      'Baccalauréat Économique et Social',
+      'Certifications',
+      'Frontend',
+      'Backend',
+      'Outils',
+      'Course à pieds',
+      'Bénévolat',
+      'Voyages'
+    ];
+    cy.get('h3').each((blockTitle, index) => {
+      expect(blockTitle.text()).to.eq(blockTitles[index]);
+    });
   });
 });
--- cypress/e2e/tests/index.spec.ts
+++ cypress/e2e/tests/index.spec.ts
@@ -5,11 +5,56 @@ describe('Portfolio tests', () => {
     cy.visit('/');
   });
 
-  it('Should get the title', () => {
+  it('Should get the home page', () => {
     cy.title()
       .should('equals', 'Arnaud Flaesch, Développeur Web')
       .get('h1')
       .first()
-      .should('have.text', 'Arnaud Flaesch');
+      .should('have.text', 'Arnaud Flaesch')
+      .get('#bio-avatar')
+      .should('have.css', 'height', '100px')
+      .and('have.css', 'width', '100px');
+
+    cy.get('footer').should('have.text', '© 2024, Développé avec Gatsby. Icons by Icons8.');
+  });
+
+  it('Should check that Linkedin link works', () => {
+    const linkedinUrl = 'https://www.linkedin.com/in/arnaudflaesch/';
+    cy.url().should('eq', Cypress.config('baseUrl'));
+    cy.get('#linkedinLink').click();
+    cy.origin(linkedinUrl, { args: { linkedinUrl } }, ({ linkedinUrl }) => {
+      cy.title().should('equal', 'Arnaud Flaesch - Publicis Sapient France | LinkedIn');
+      cy.url().should('equals', linkedinUrl);
+    });
+  });
+
+  it('Should check that Github link works', () => {
+    const githubUrl = 'https://github.com/ArnaudFlaesch';
+    cy.url().should('eq', Cypress.config('baseUrl'));
+    cy.get('#githubLink').click();
+    cy.origin(githubUrl, { args: { githubUrl } }, ({ githubUrl }) => {
+      cy.title().should('equal', 'ArnaudFlaesch (Arnaud Flaesch) · GitHub');
+      cy.url().should('equals', githubUrl);
+    });
+  });
+
+  it('Should check that Scrum.org link works', () => {
+    const scrumOrgUrl = 'https://www.scrum.org/user/1355891';
+    cy.url().should('eq', Cypress.config('baseUrl'));
+    cy.get('#scrumOrgLink').click();
+    cy.origin(scrumOrgUrl, { args: { scrumOrgUrl } }, ({ scrumOrgUrl }) => {
+      cy.title().should('equal', 'Arnaud Flaesch | Scrum.org');
+      cy.url().should('equals', scrumOrgUrl);
+    });
+  });
+
+  it('Should check that Medium link works', () => {
+    const mediumUrl = 'https://medium.com/@arnaudflaesch';
+    cy.url().should('eq', Cypress.config('baseUrl'));
+    cy.get('#mediumLink').click();
+    cy.origin(mediumUrl, { args: { mediumUrl } }, ({ mediumUrl }) => {
+      cy.title().should('equal', 'Arnaud Flaesch – Medium');
+      cy.url().should('equals', mediumUrl);
+    });
   });
 });
