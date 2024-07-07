e116f69bb8fbe0134720fddd2e29e611da683c36
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2024-07-07 21:04:57 +0200

    Update CSS

--- cypress/e2e/tests/blog.spec.ts
+++ cypress/e2e/tests/blog.spec.ts
@@ -2,7 +2,7 @@
 
 describe('Blog page tests', () => {
   it('Should display the blog page', () => {
-    cy.get('#portfolio-header > #url-list > a').contains('Blog').click();
+    cy.get('#portfolio-header #url-list > a').contains('Blog').click();
     cy.title().should('equals', "Blog - Arnaud Flaesch, Développeur d'applications");
   });
 });
--- cypress/e2e/tests/contact.spec.ts
+++ cypress/e2e/tests/contact.spec.ts
@@ -2,7 +2,7 @@
 
 describe('Contact page tests', () => {
   it('Should display the contact page', () => {
-    cy.get('#portfolio-header > #url-list > a').contains('Contact').click();
+    cy.get('#portfolio-header #url-list > a').contains('Contact').click();
     cy.title().should('equals', "Contactez-moi - Arnaud Flaesch, Développeur d'applications");
   });
 });
