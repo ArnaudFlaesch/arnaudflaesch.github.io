6744e5ac5ebccc7d980d3a7e870260cd72e79185
Arnaud Flaesch <ArnaudFlaesch@users.noreply.github.com>
2024-07-12 21:21:43 +0200

    Update gatsby-config.js (#1219)

--- cypress/e2e/tests/projects.spec.ts
+++ cypress/e2e/tests/projects.spec.ts
@@ -3,6 +3,6 @@
 describe('Projects page tests', () => {
   it('Should display the projects page', () => {
     cy.get('#portfolio-header #url-list > a').contains('Projets').click();
-    cy.title().should('equals', "Projets personnels - Arnaud Flaesch, Développeur d'applications");
+    cy.title().should('equals', "Projets personnels - Arnaud Flaesch - Développeur d'applications");
   });
 });
