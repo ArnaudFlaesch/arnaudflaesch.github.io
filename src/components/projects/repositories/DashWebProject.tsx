866ebb988a0e3b5799684d8fc343ce06fbf98c57
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2024-09-06 12:00:58 +0200

    Refactoring

--- src/components/projects/repositories/DashWebProject.tsx
+++ src/components/projects/repositories/DashWebProject.tsx
@@ -1,15 +1,11 @@
 import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 import { IRepository } from '../../../model/IRepository';
+import { AngularIcon, CypressIcon, JestIcon, SassIcon } from './LanguagesIcons';
 import RepositoryWidget from './RepositoryWidget';
-import { StaticImage } from 'gatsby-plugin-image';
-import TooltipIcon from '../../tooltip-icon/TooltipIcon';
 
 export default function DashWebProject(): React.ReactElement {
   const description = 'DASH.WEB.PROJECT.DESCRIPTION';
-
-  const ICON_HEIGHT = 40;
-  const ICON_WIDTH = 40;
   const QUERY_DASH_WEB = graphql`
     {
       github {
@@ -33,44 +29,10 @@ export default function DashWebProject(): React.ReactElement {
 
   const dashWebRepo: IRepository = useStaticQuery(QUERY_DASH_WEB).github.repository;
   const repositoryIcons = [
-    <TooltipIcon
-      key={'Angular 2'}
-      tooltip="Angular 2"
-      image={
-        <StaticImage
-          src="../../../images/icons/frontend/angular2.svg"
-          width={ICON_WIDTH}
-          height={ICON_HEIGHT}
-          alt="Angular 2"
-        />
-      }
-    />,
-    <TooltipIcon
-      key={'Jest'}
-      tooltip="Jest"
-      image={
-        <StaticImage src="../../../images/icons/frontend/jest.svg" width={ICON_WIDTH} height={ICON_HEIGHT} alt="Jest" />
-      }
-    />,
-    <TooltipIcon
-      key={'Sass'}
-      tooltip="Sass"
-      image={
-        <StaticImage src="../../../images/icons/frontend/sass.svg" width={ICON_WIDTH} height={ICON_HEIGHT} alt="Sass" />
-      }
-    />,
-    <TooltipIcon
-      key={'Cypress'}
-      tooltip="Cypress"
-      image={
-        <StaticImage
-          src="../../../images/icons/frontend/cypress.png"
-          width={ICON_WIDTH}
-          height={ICON_HEIGHT}
-          alt="Cypress"
-        />
-      }
-    />
+    <AngularIcon key={'Angular 2'} />,
+    <JestIcon key={'Jest'} />,
+    <SassIcon key={'Sass'} />,
+    <CypressIcon key={'Cypress'} />
   ];
 
   return (
--- src/components/projects/repositories/PortfolioProject.tsx
+++ src/components/projects/repositories/PortfolioProject.tsx
@@ -1,14 +1,11 @@
 import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 import { IRepository } from '../../../model/IRepository';
+import { GatsbyIcon, GithubActionsIcon, GraphQLIcon, ReactJSIcon, SassIcon, TypeScriptIcon } from './LanguagesIcons';
 import RepositoryWidget from './RepositoryWidget';
-import { StaticImage } from 'gatsby-plugin-image';
-import TooltipIcon from '../../tooltip-icon/TooltipIcon';
 
 export default function PortfolioProject(): React.ReactElement {
   const description = 'PORTFOLIO.PROJECT.DESCRIPTION';
-  const ICON_HEIGHT = 40;
-  const ICON_WIDTH = 40;
   const QUERY = graphql`
     {
       github {
@@ -32,74 +29,12 @@ export default function PortfolioProject(): React.ReactElement {
 
   const portfolioRepo: IRepository = useStaticQuery(QUERY).github.repository;
   const repositoryIcons = [
-    <TooltipIcon
-      key={'ReactJS'}
-      tooltip="ReactJS"
-      image={
-        <StaticImage
-          src="../../../images/icons/frontend/reactjs.png"
-          width={ICON_WIDTH}
-          height={ICON_HEIGHT}
-          alt="ReactJS"
-        />
-      }
-    />,
-    <TooltipIcon
-      key={'Sass'}
-      tooltip="Sass"
-      image={
-        <StaticImage src="../../../images/icons/frontend/sass.svg" width={ICON_WIDTH} height={ICON_HEIGHT} alt="Sass" />
-      }
-    />,
-
-    <TooltipIcon
-      key={'Gatsby'}
-      tooltip="Gatsby"
-      image={
-        <StaticImage
-          src="../../../images/icons/frontend/gatsby.svg"
-          width={ICON_WIDTH}
-          height={ICON_HEIGHT}
-          alt="Gatsby"
-        />
-      }
-    />,
-    <TooltipIcon
-      key={'TypeScript'}
-      tooltip="TypeScript"
-      image={
-        <StaticImage
-          src="../../../images/icons/frontend/typescript.svg"
-          width={ICON_WIDTH}
-          height={ICON_HEIGHT}
-          alt="TypeScript"
-        />
-      }
-    />,
-    <TooltipIcon
-      key={'Github Actions'}
-      tooltip="Github Actions"
-      image={
-        <StaticImage
-          src="../../../images/icons/tools/github-actions.png"
-          width={ICON_WIDTH}
-          height={ICON_HEIGHT}
-          alt="Github Actions"
-        />
-      }
-    />,
-    <TooltipIcon
-      key={'GraphQL'}
-      tooltip="GraphQL"
-      image={
-        <StaticImage
-          src="../../../images/icons/tools/graphql.png"
-          width={ICON_WIDTH}
-          height={ICON_HEIGHT}
-          alt="GraphQL"
-        />
-      }
-    />
+    <ReactJSIcon key={'ReactJS'} />,
+    <SassIcon key={'Jest'} />,
+    <GatsbyIcon key={'Gatsby'} />,
+    <TypeScriptIcon key={'TypeScript'} />,
+    <GithubActionsIcon key={'Github Actions'} />,
+    <GraphQLIcon key={'GraphQL'} />
   ];
 
   return (
